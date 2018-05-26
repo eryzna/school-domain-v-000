ed8ff6d64e59684da1c95ea640f550c45c8afa9a
Elana Ryznar <eryzna@gmail.com>
2018-05-26 21:39:39 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,7 @@ class School
   def add_student(name,grade)
     roster[grade]=[]
     if roster[grade].include? "#{grade}"
-      roster[grade]<< name
+      roster[grade][name]<<name
     else
       roster[grade]= [name]
     end#unless roster[grade].include? "#{grade}"
